67317f1e1cec179e798bd5045eb95f2ad4bf05c1
Daniel Sharp <dannys4@mit.edu>
2024-04-09 13:12:39 -0400

    Try adding docs

---
+++ docs/make.jl
@@ -0,0 +1,25 @@
+using MultiIndexing
+using Documenter
+
+DocMeta.setdocmeta!(MultiIndexing, :DocTestSetup, :(using MultiIndexing); recursive=true)
+
+makedocs(;
+    modules=[MultiIndexing],
+    authors="Daniel Sharp",
+    repo="https://github.com/dannys4/MultiIndexing.jl/blob/{commit}{path}#{line}",
+    sitename="MultiIndexing.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://dannys4.github.io/MultiIndexing.jl",
+        edit_link="main",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md"
+    ],
+)
+
+deploydocs(;
+    repo="github.com/dannys4/MultiIndexing.jl",
+    devbranch="addDocs",
+)
