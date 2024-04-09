6d57498398ab6b62dbce8a57d3d501dd0c676e2a
Daniel Sharp <dannys4@mit.edu>
2024-04-09 11:32:25 -0400

    Adding first code

--- src/MultiIndexing.jl
+++ src/MultiIndexing.jl
@@ -1,4 +1,249 @@
 module MultiIndexing
 
+using StaticArrays, LinearAlgebra
+import Base: getindex, push!
+
+# Represents a set of multi-indices
+# N x dim matrix (dim is dimension of multi-indices, N is number)
+struct MultiIndexSet{d,T}
+    indices::Vector{StaticVector{d,Int}}
+    reduced_margin::Vector{StaticVector{d,Int}}
+    limit::T
+    isDownwardClosed::Bool
+    maxDegrees::MVector{d,Int}
+end
+
+# indices: dim x N matrix of multi-indices
+# limit: function that takes a multi-index and a limit and returns true if the index is admissible
+# calc_reduced_margin: if true, calculate the reduced margin of the set, otherwise leave it empty
+function MultiIndexSet(indices_mat::Matrix{Int}, limit::T=NoLimiter, calc_reduced_margin=false) where {T}
+    d = size(indices_mat,1)
+    indices = [SVector{d}(indices_mat[:,i]) for i in axes(indices_mat,2)]
+    reduced_margin = []
+    if calc_reduced_margin
+        throw(ArgumentError("Reduced margin calculation not implemented"))
+    end
+    maxDegrees = MVector{d}(maximum(indices_mat, dims=2)[:])
+    MultiIndexSet{d,T}(indices, reduced_margin, limit, true, maxDegrees)
+end
+
+function Base.getindex(mis::MultiIndexSet{d}, i::Int) where {d}
+    mis.indices[i]
+end
+
+# Creates a matrix of multi-indices of total order p
+# returns the matrix and the index where the frontier starts
+function CreateTotalOrder_matrix(d::Int, p::Int)
+    Mk = zeros(Int, d, 1)
+    M = Mk
+    last_start = -1
+    for j in 1:p
+        Mk = repeat(Mk, 1, d) + kron(I(d), ones(Int, 1, size(Mk, 2)))
+        Mk = unique(Mk, dims=2)
+        M = hcat(M, Mk)
+        j == p-1 && (last_start = size(M, 2)+1)
+    end
+    M, last_start
+end
+
+NoLimiter = Returns(true)
+
+SumLimiter = (x::StaticVector,p) -> sum(x) <= p
+
+struct AnisotropicLimiter{d,T}
+    weights::StaticVector{d,Float64}
+    limit::T
+    function AnisotropicLimiter(weights::AbstractVector{Float64}, limit::_T = SumLimiter) where {_T}
+        _d = length(weights)
+        new{_d,_T}(SVector{_d}(weights), limit)
+    end
+end
+
+function (lim::AnisotropicLimiter{d})(index::StaticVector{d}, p) where {d}
+    lim.limit(index .* lim.weights, p)
+end
+
+struct CurvedLimiter{d,T}
+    curve_weights::StaticVector{d,Float64}
+    limit::T
+    function CurvedLimiter(curve_weights::AbstractVector{Float64}, limit::_T = SumLimiter) where {_T}
+        _d = length(curve_weights)
+        new{_d,_T}(SVector{_d}(curve_weights), limit)
+    end
+end
+
+function (lim::CurvedLimiter{d})(index::StaticVector{d}, p) where {d}
+    lim.limit(index + lim.curve_weights .* log1p.(index), p)
+end
+
+function CreateTotalOrder(d::Int, p, limit=NoLimiter)
+    mset_mat, last_start = CreateTotalOrder_matrix(d, ceil(p))
+    frontier = @view mset_mat[:, last_start:end]
+    indices = [SVector{d}(mset_mat[:,i]) for i in axes(mset_mat,2) if limit(SVector{d}(mset_mat[:,i]), p)]
+    reduced_margin = Vector{StaticVector{d,Int}}(undef, size(frontier, 2)*d)
+    rm_idx = 1
+    max_degrees = zeros(Int, d)
+    full_limited = true
+    @inbounds for i in axes(frontier, 2)
+        m_idx = frontier[:,i]
+        limit(SVector{d}(m_idx), p) && for j in 1:d
+            max_degrees[j] = max(max_degrees[j], m_idx[j])
+            m_idx[j] += 1
+            static_m_idx = SVector{d}(m_idx)
+            if limit(static_m_idx, p+1)
+                reduced_margin[rm_idx] = static_m_idx
+                rm_idx += 1
+            end
+            m_idx[j] -= 1
+            full_limited &= false;
+        end
+    end
+    full_limited && @warn "No valid reduced margin found on frontier"
+    MultiIndexSet{d,typeof(limit)}(indices, unique(reduced_margin[1:rm_idx-1]), limit, true, SVector{d}(max_degrees))
+end
+
+function tens_prod_mat(idx)
+    reduce(hcat, collect.(Tuple.(vec(CartesianIndices(ntuple(k->0:idx[k], length(idx)))))))
+end
+function tens_prod_mat(p,d)
+    tens_prod_mat(fill(p,d))
+end
+
+function CreateTensorOrder(d::Int, p::Int, limit::T=NoLimiter) where {T}
+    indices_arr = CartesianIndices(ntuple(_->0:p, d))
+    indices = [SVector(Tuple(idx)) for idx in vec(indices_arr) if limit(SVector(Tuple(idx)), p)]
+    reduced_margin_arr = CartesianIndices(ntuple(_->0:p+1, d))
+    reduced_margin = [SVector(Tuple(idx)) for idx in vec(reduced_margin_arr) if any(idx .> p) && limit(SVector(Tuple(idx)), p+1)]
+    MultiIndexSet{d,T}(indices, reduced_margin, limit, true, SVector{d}(fill(p,d)))
+end
+
+# Searches backward through the mset backward for an index
+function in_mset_backward(mis::MultiIndexSet{d}, idx::StaticVector{d,Int}) where {d}
+    any(idx .> mis.maxDegrees) && return false
+    for i in length(mis.indices):-1:1
+        mis.indices[i] == idx && return true
+    end
+    false
+end
+
+# Check if an index is admissible to add to a reduced margin
+function checkIndexAdmissible(mis::MultiIndexSet{d}, idx::StaticVector{d,Int}, check_indices::Bool = false) where {d}
+    # If any index is greater than the limit, return false
+    mis.limit(idx) && return false
+    # If more than one subindex is greater than its max degree, return false
+    sum(idx .>= mis.maxDegrees .+ 1) > 1 && return false
+    # Check if the index is in the reduced margin
+    idx in mis.reduced_margin && return false
+    if check_indices # Check if the index is in the indices (expensive)
+        idx in mis.indices && return false
+    end
+    # Otherwise, check that all backward neighbors of the index is in the set
+    idx_tmp = Vector(idx)
+    for i in 1:d
+        idx[i] == 0 && continue
+        idx_tmp[i] -= 1
+        !(SVector{d}(idx_tmp) in mis.indices) && return false
+        idx_tmp[i] += 1
+    end
+    true
+end
+
+function allBackwardAncestors(mis::MultiIndexSet{d}, j::Int) where {d}
+    idx = mis.indices[j]
+    back_indices = CartesianIndices(ntuple(k->0:idx[k], d))
+    j_ancestors = Vector{Int}(undef, length(back_indices) - 1)
+    for (i, back_index) in enumerate(back_indices)
+        if i < length(back_indices)
+            check_back_index = isequal(SVector(Tuple(back_index)))
+            j_ancestors[i] = findfirst(check_back_index, mis.indices)
+        end
+    end
+    j_ancestors
+end
+
+# Update the max degrees of a set after adding an index
+function updateMaxDegrees!(mis::MultiIndexSet{d}, idx::StaticVector{d,Int}) where {d}
+    mis.maxDegrees .= max.(mis.maxDegrees, idx)
+end
+
+# Update the reduced margin of a set after adding an index
+function updateReducedMargin!(mis::MultiIndexSet{d}, idx::StaticVector{d,Int}) where {d}
+    j = findfirst(isequal(idx), mis.reduced_margin)
+    # delete idx from reduced margin
+    deleteat!(mis.reduced_margin, j)
+    # for each neighbor of idx, check valid and add to reduced margin
+    idx_tmp = Vector(idx)
+    for i in 1:d
+        idx_tmp[i] += 1
+        tmp_i = SVector{d}(idx_tmp)
+        if checkIndexValid(mis, tmp_i, false)
+            push!(mis.reduced_margin, tmp_i)
+        end
+        idx_tmp[i] -= 1
+    end
+end
+
+function push!(mis::MultiIndexSet{d}, idx::StaticVector{d,Int}) where {d}
+    (idx in mis.indices) && return true
+    isValid = checkIndexValid(mis, idx)
+    mis.isDownwardClosed = isValid
+    push!(mis.indices, idx)
+    updateMaxDegrees!(mis, idx)
+    # Update reduced margin if downward closed
+    if mis.isDownwardClosed
+        updateReducedMargin!(mis, idx)
+    end
+    isValid
+end
+
+function findReducedFrontier(mis::MultiIndexSet{d}) where {d}
+    frontier = Int[]
+    unmarked = ones(Bool, length(mis.indices))
+    sorted_idxs = sortperm(mis.indices, lt=(x,y)->sum(x) < sum(y))
+    inv_sorted_idxs = invperm(sorted_idxs)
+    while true
+        last_unmarked = findlast(unmarked)
+        isnothing(last_unmarked) && break
+        # Mark all backward ancestors of last_unmarked
+        ancestors = allBackwardAncestors(mis, sorted_idxs[last_unmarked])
+        # @assert any(ancestors .> last_unmarked) "Unmarked index has forward ancestor"
+        unmarked[inv_sorted_idxs[ancestors]] .= false
+        unmarked[last_unmarked] = false
+        push!(frontier, sorted_idxs[last_unmarked])
+    end
+    frontier
+end # End MultiIndexing
+
+# Gets the smolyak indexing for a multi-index set. Each index represents a tensor-product box.
+# If keep_levels is true, returns a vector of vectors of indices at each level of the Smolyak grid,
+# where index j of the return value corresponds to subtracted tensor products if j is even and
+# positive if j is odd.
+# If keep_levels is false, returns a pair of vectors, where the first is the positive tensor-product quadratures
+# and the second is the subtracted tensor-product quadratures.
+function smolyakIndexing(mis::MultiIndexSet{d,T}, keep_levels::Bool=false) where {d,T}
+    mi_loop = mis
+    frontiers = keep_levels ? Vector{Int}[] : ntuple(_->Int[], 2)
+    original_indices = collect(1:length(mis.indices))
+    frontiers_index = 1
+    while length(mi_loop.indices) > 0
+        frontier = findReducedFrontier(mi_loop)
+        occurrences = zeros(Int, length(mi_loop.indices))
+        for idx in frontier
+            back_idxs = allBackwardAncestors(mi_loop, idx)
+            occurrences[back_idxs] .+= 1
+        end
+        sort!(frontier)
+        if keep_levels
+            push!(frontiers, original_indices[frontier])
+        else
+            append!(frontiers[frontiers_index], original_indices[frontier])
+        end
+        repeated_indices = mi_loop.indices[occurrences .> 1]
+        original_indices = original_indices[occurrences .> 1]
+        mi_loop = MultiIndexSet{d,T}(repeated_indices, [], mis.limit, true, mis.maxDegrees)
+        frontiers_index = 3-frontiers_index # map 2 to 1 and 1 to 2
+    end
+    frontiers
+end
 
 end
