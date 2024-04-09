a629cce5a5f5f5a6d1efebe92a5bc9fcd27fc011
Documenter.jl <documenter@juliadocs.github.io>
2024-04-09 17:48:10 +0000

    build based on a2de016

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MultiIndexing","category":"page"},{"location":"#MultiIndexing","page":"Home","title":"MultiIndexing","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MultiIndexing.jl.","category":"page"}]
+}
